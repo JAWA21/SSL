7dbb42840ea25b80f64245bde4e8b22265368daa
Jeanna <jawa@fullsail.edu>
2013-10-03 09:39:42 -0400

    first commit
    Day 1 lab

---
+++ day1/index.php
@@ -0,0 +1,6 @@
+<? include 'views/header.php';?>
+<? include 'views/nav.php';?>
+
+<? include 'views/body.php';?>
+
+<? include 'views/footer.php';?>
---
+++ day1/views/nav.php
@@ -0,0 +1,10 @@
+<aside>
+	<nav>
+	  <ul>
+	    <li><a href="#">Home</a></li>
+	    <li><a href="#">About</a></li>
+	    <li><a href="#">Videos</a></li>
+	    <li><a href="#">Register</a></li>
+	  </ul>
+	</nav>
+</aside>
